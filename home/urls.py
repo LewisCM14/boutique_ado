2d38f1e00e86dd9157c3d9ebda816f0dbe328e76
LewisCM14 <Lewiscmartin@live.co.uk>
2022-03-08 17:55:42 +0000

    added products views and templates

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,5 @@
 """ This module contains the urls for the home app """
 
-from django.contrib import admin
 from django.urls import path
 from . import views
 
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+""" This module containts the views for the products app. """
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Product
+
+
+def all_products(request):
+    """
+    A view to show all products.
+    Also shows sorting and search queries.
+    """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
