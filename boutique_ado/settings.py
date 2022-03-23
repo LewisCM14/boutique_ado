6d0f72d316d5128534dc70dc26827f10d3e0c746
LewisCM14 <Lewiscmartin@live.co.uk>
2022-03-23 22:45:14 +0000

    update allowed hosts

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -13,19 +13,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+
 if os.path.isfile('env.py'):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-x_m7$92+1ycr^1wy-r8^2c3%l9jpql$rxe*j4tdgef%0^hk#)g'  # noqa
-# SECRET_KEY = os.environ.get('SECRET_KEY')  # noqa
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Development Variables
 # development = os.environ.get('DEVELOPMENT', False)
@@ -34,7 +36,7 @@ SECRET_KEY = 'django-insecure-x_m7$92+1ycr^1wy-r8^2c3%l9jpql$rxe*j4tdgef%0^hk#)g
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['boutique-ado-lewiscm.herokuapp.com', 'localhost', ]
 
 # DEBUG = development
 
@@ -136,25 +138,21 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# when the app is running on Heroku
+# where the database URL environment variable will be defined.
+# We connect to Postgres and otherwise, we connect to sequel light.
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
-# if development:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': BASE_DIR / 'db.sqlite3',
-#         }
-#     }
-# else:
-#     DATABASES = {
-#         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-#     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
